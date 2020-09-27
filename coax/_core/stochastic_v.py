6b3ca57ec6516d3b03e70920b786253765a9ed15
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2020-09-27 13:19:55 +1000

    add StochasticV

---
+++ coax/_core/stochastic_v.py
@@ -0,0 +1,223 @@
+# ------------------------------------------------------------------------------------------------ #
+# MIT License                                                                                      #
+#                                                                                                  #
+# Copyright (c) 2020, Microsoft Corporation                                                        #
+#                                                                                                  #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software    #
+# and associated documentation files (the "Software"), to deal in the Software without             #
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,       #
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the    #
+# Software is furnished to do so, subject to the following conditions:                             #
+#                                                                                                  #
+# The above copyright notice and this permission notice shall be included in all copies or         #
+# substantial portions of the Software.                                                            #
+#                                                                                                  #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING    #
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND       #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,     #
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          #
+# ------------------------------------------------------------------------------------------------ #
+
+from gym.spaces import Box
+
+from ..proba_dists import ProbaDist, DiscretizedIntervalDist
+from ..value_transforms import ValueTransform
+from .base_stochastic_func_type2 import BaseStochasticFuncType2
+
+
+__all__ = (
+    'StochasticV',
+)
+
+
+class StochasticV(BaseStochasticFuncType2):
+    r"""
+
+    A state-value function :math:`v(s)`, represented by a stochastic function
+    :math:`\mathbb{P}_\theta(G_t|S_t=s)`.
+
+    Parameters
+    ----------
+    func : function
+
+        A Haiku-style function that specifies the forward pass.
+
+    env : gym.Env
+
+        The gym-style environment. This is used to validate the input/output structure of ``func``.
+
+    value_range : tuple of floats
+
+        A pair of floats :code:`(min_value, max_value)`.
+
+    num_bins : int, optional
+
+        If provided, the space of values is discretized into :code:`num_bins` equal-sized bins. This
+        also changes the underlying probability distribution to a :class:`CategoricalDist
+        <coax.proba_dists.CategoricalDist>` instead of the unimodal :class:`NormalDist
+        <coax.proba_dists.NormalDist>`.
+
+    observation_preprocessor : function, optional
+
+        Turns a single observation into a batch of observations that are compatible with the
+        corresponding probability distribution. If left unspecified, this defaults to:
+
+        .. code:: python
+
+            observation_preprocessor = ProbaDist(observation_space).preprocess_variate
+
+        See also :attr:`coax.proba_dists.ProbaDist.preprocess_variate`.
+
+    value_transform : ValueTransform or pair of funcs, optional
+
+        If provided, the target for the underlying function approximator is transformed:
+
+        .. math::
+
+            \tilde{G}_t\ =\ f(G_t)
+
+        This means that calling the function involves undoing this transformation using its inverse
+        :math:`f^{-1}`. The functions :math:`f` and :math:`f^{-1}` are given by
+        ``value_transform.transform_func`` and ``value_transform.inverse_func``, respectively. Note
+        that a ValueTransform is just a glorified pair of functions, i.e. passing
+        ``value_transform=(func, inverse_func)`` works just as well.
+
+    random_seed : int, optional
+
+        Seed for pseudo-random number generators.
+
+    """
+    def __init__(
+            self, func, env, value_range, num_bins=51, observation_preprocessor=None,
+            value_transform=None, random_seed=None):
+
+        self.value_transform = value_transform
+        self.value_range = self._check_value_range(value_range)
+        proba_dist = self._get_proba_dist(self.value_range, num_bins)
+
+        # set defaults
+        if observation_preprocessor is None:
+            observation_preprocessor = ProbaDist(env.observation_space).preprocess_variate
+        if self.value_transform is None:
+            self.value_transform = ValueTransform(lambda x: x, lambda x: x)
+        if not isinstance(self.value_transform, ValueTransform):
+            self.value_transform = ValueTransform(*value_transform)
+
+        super().__init__(
+            func=func,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            observation_preprocessor=observation_preprocessor,
+            proba_dist=proba_dist,
+            random_seed=random_seed)
+
+    @property
+    def num_bins(self):
+        return self.proba_dist.space.n
+
+    @classmethod
+    def example_data(
+            cls, env, value_range, num_bins=51, observation_preprocessor=None,
+            batch_size=1, random_seed=None):
+
+        value_range = cls._check_value_range(value_range)
+        proba_dist = cls._get_proba_dist(value_range, num_bins)
+
+        if observation_preprocessor is None:
+            observation_preprocessor = ProbaDist(env.observation_space).preprocess_variate
+
+        return super().example_data(
+            env=env,
+            observation_preprocessor=observation_preprocessor,
+            proba_dist=proba_dist,
+            batch_size=batch_size,
+            random_seed=random_seed)
+
+    def __call__(self, s, return_logp=False):
+        r"""
+
+        Sample a value.
+
+        Parameters
+        ----------
+        s : state observation
+
+            A single state observation :math:`s`.
+
+        return_logp : bool, optional
+
+            Whether to return the log-propensity associated with the sampled output value.
+
+        Returns
+        -------
+        value : float or list thereof
+
+            Depending on whether :code:`a` is provided, this either returns a single value or a list
+            of :math:`n` values, one for each discrete action.
+
+        logp : non-positive float or list thereof, optional
+
+            The log-propensity associated with the sampled output value. This is only returned if we
+            set ``return_logp=True``. Depending on whether :code:`a` is provided, this is either a
+            single float or a list of :math:`n` floats, one for each discrete action.
+
+        """
+        return super().__call__(s, return_logp=return_logp)
+
+    def mode(self, s):
+        r"""
+
+        Get the most probable value.
+
+        Parameters
+        ----------
+        s : state observation
+
+            A single state observation :math:`s`.
+
+        Returns
+        -------
+        value : float or list thereof
+
+            Depending on whether :code:`a` is provided, this either returns a single value or a list
+            of :math:`n` values, one for each discrete action.
+
+        """
+        return super().mode(s)
+
+    def dist_params(self, s):
+        r"""
+
+        Get the parameters of the underlying (conditional) probability distribution.
+
+        Parameters
+        ----------
+        s : state observation
+
+            A single state observation :math:`s`.
+
+        Returns
+        -------
+        dist_params : dict or list of dicts
+
+            Depending on whether :code:`a` is provided, this either returns a single dist-params
+            dict or a list of :math:`n` such dicts, one for each discrete action.
+
+        """
+        return super().dist_params(s)
+
+    @staticmethod
+    def _get_proba_dist(value_range, num_bins):
+        reward_space = Box(*value_range, shape=())
+        return DiscretizedIntervalDist(reward_space, num_bins)
+
+    @staticmethod
+    def _check_value_range(value_range):
+        if not (isinstance(value_range, (tuple, list))
+                and len(value_range) == 2
+                and isinstance(value_range[0], (int, float))
+                and isinstance(value_range[1], (int, float))
+                and value_range[0] < value_range[1]):
+            raise TypeError("value_range is not a valid pair tuple of floats: (low, high)")
+        return float(value_range[0]), float(value_range[1])
---
+++ coax/_core/stochastic_v_test.py
@@ -0,0 +1,152 @@
+# ------------------------------------------------------------------------------------------------ #
+# MIT License                                                                                      #
+#                                                                                                  #
+# Copyright (c) 2020, Microsoft Corporation                                                        #
+#                                                                                                  #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software    #
+# and associated documentation files (the "Software"), to deal in the Software without             #
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,       #
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the    #
+# Software is furnished to do so, subject to the following conditions:                             #
+#                                                                                                  #
+# The above copyright notice and this permission notice shall be included in all copies or         #
+# substantial portions of the Software.                                                            #
+#                                                                                                  #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING    #
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND       #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,     #
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          #
+# ------------------------------------------------------------------------------------------------ #
+
+from functools import partial
+from collections import namedtuple
+
+import jax
+import jax.numpy as jnp
+import haiku as hk
+from gym.spaces import Discrete, Box
+
+from .._base.test_case import TestCase
+from ..utils import safe_sample
+from .stochastic_v import StochasticV
+
+discrete = Discrete(7)
+boxspace = Box(low=0, high=1, shape=(3, 5))
+num_bins = 20
+
+Env = namedtuple('Env', ('observation_space', 'action_space'))
+
+
+def func(S, is_training):
+    batch_norm = hk.BatchNorm(False, False, 0.99)
+    logits = hk.Sequential((
+        hk.Flatten(),
+        hk.Linear(8), jax.nn.relu,
+        partial(hk.dropout, hk.next_rng_key(), 0.25 if is_training else 0.),
+        partial(batch_norm, is_training=is_training),
+        hk.Linear(8), jnp.tanh,
+        hk.Linear(num_bins),
+    ))
+    return {'logits': logits(S)}
+
+
+class TestStochasticV(TestCase):
+    def test_init(self):
+        StochasticV(func, Env(boxspace, boxspace), (-10, 10), num_bins=num_bins)
+        StochasticV(func, Env(boxspace, discrete), (-10, 10), num_bins=num_bins)
+        StochasticV(func, Env(discrete, boxspace), (-10, 10), num_bins=num_bins)
+        StochasticV(func, Env(discrete, discrete), (-10, 10), num_bins=num_bins)
+
+    # test_call_* ##################################################################################
+
+    def test_call_discrete(self):
+        env = Env(discrete, discrete)
+        value_range = (-10, 10)
+
+        s = safe_sample(env.observation_space, seed=17)
+        v = StochasticV(func, env, value_range, num_bins=num_bins, random_seed=19)
+
+        v_, logp = v(s, return_logp=True)
+        print(v_, logp, env.observation_space)
+        self.assertIn(v_, Box(*value_range, shape=()))
+        self.assertArraySubdtypeFloat(logp)
+        self.assertArrayShape(logp, ())
+
+    def test_call_boxspace(self):
+        env = Env(boxspace, discrete)
+        value_range = (-10, 10)
+
+        s = safe_sample(env.observation_space, seed=17)
+        v = StochasticV(func, env, value_range, num_bins=num_bins, random_seed=19)
+
+        v_, logp = v(s, return_logp=True)
+        print(v_, logp, env.observation_space)
+        self.assertIn(v_, Box(*value_range, shape=()))
+        self.assertArraySubdtypeFloat(logp)
+        self.assertArrayShape(logp, ())
+
+    # test_mode_* ##################################################################################
+
+    def test_mode_discrete(self):
+        env = Env(discrete, discrete)
+        value_range = (-10, 10)
+
+        s = safe_sample(env.observation_space, seed=17)
+        v = StochasticV(func, env, value_range, num_bins=num_bins, random_seed=19)
+
+        v_ = v.mode(s)
+        print(v_, env.observation_space)
+        self.assertIn(v_, Box(*value_range, shape=()))
+
+    def test_mode_boxspace(self):
+        env = Env(boxspace, discrete)
+        value_range = (-10, 10)
+
+        s = safe_sample(env.observation_space, seed=17)
+        v = StochasticV(func, env, value_range, num_bins=num_bins, random_seed=19)
+
+        v_ = v.mode(s)
+        print(v_, env.observation_space)
+        self.assertIn(v_, Box(*value_range, shape=()))
+
+    def test_function_state(self):
+        env = Env(discrete, discrete)
+        value_range = (-10, 10)
+
+        v = StochasticV(func, env, value_range, num_bins=num_bins, random_seed=19)
+
+        print(v.function_state)
+        batch_norm_avg = v.function_state['batch_norm/~/mean_ema']['average']
+        self.assertArrayShape(batch_norm_avg, (1, 8))
+        self.assertArrayNotEqual(batch_norm_avg, jnp.zeros_like(batch_norm_avg))
+
+    # other tests ##################################################################################
+
+    def test_bad_input_signature(self):
+        def badfunc(S, is_training, x):
+            pass
+        msg = (
+            r"func has bad signature; "
+            r"expected: func\(S, is_training\), "
+            r"got: func\(S, is_training, x\)"
+        )
+        with self.assertRaisesRegex(TypeError, msg):
+            env = Env(boxspace, discrete)
+            value_range = (-10, 10)
+            StochasticV(badfunc, env, value_range, num_bins=num_bins, random_seed=13)
+
+    def test_bad_output_structure(self):
+        def badfunc(S, is_training):
+            dist_params = func(S, is_training)
+            dist_params['foo'] = jnp.zeros(1)
+            return dist_params
+        msg = (
+            r"func has bad return tree_structure, "
+            r"expected: PyTreeDef\(dict\[\['logits'\]\], \[\*\]\), "
+            r"got: PyTreeDef\(dict\[\['foo', 'logits'\]\], \[\*,\*\]\)"
+        )
+        with self.assertRaisesRegex(TypeError, msg):
+            env = Env(discrete, discrete)
+            value_range = (-10, 10)
+            StochasticV(badfunc, env, value_range, num_bins=num_bins, random_seed=13)
