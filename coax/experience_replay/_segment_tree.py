08029b92889d036f96f23017758bd6f9e50e2da8
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2020-10-13 18:39:55 +1100

    implement SegmentTree

---
+++ coax/experience_replay/_segment_tree.py
@@ -0,0 +1,234 @@
+# ------------------------------------------------------------------------------------------------ #
+# MIT License                                                                                      #
+#                                                                                                  #
+# Copyright (c) 2020, Microsoft Corporation                                                        #
+#                                                                                                  #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software    #
+# and associated documentation files (the "Software"), to deal in the Software without             #
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,       #
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the    #
+# Software is furnished to do so, subject to the following conditions:                             #
+#                                                                                                  #
+# The above copyright notice and this permission notice shall be included in all copies or         #
+# substantial portions of the Software.                                                            #
+#                                                                                                  #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING    #
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND       #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,     #
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          #
+# ------------------------------------------------------------------------------------------------ #
+
+import numpy as onp
+
+
+__all__ = (
+    'SegmentTree',
+    'SumTree',
+    'MinTree',
+)
+
+
+class SegmentTree:
+    def __init__(self, capacity, reducer, init_value):
+        r"""
+
+        A `segment tree <https://en.wikipedia.org/wiki/Segment_tree>`_ data structure that allows
+        for batched sampling and updating.
+
+        Parameters
+        ----------
+        capacity : positive int
+
+            Number of values to accommodate.
+
+        reducer : function
+
+            The reducer function :math:`f: (float, float) \to float`.
+
+        init_value : float
+
+            The unit element relative to the reducer function. Some typical examples are: 0 if
+            reducer is :func:`operator.add`, 1 for :func:`operator.mul`, :math:`-\infty` for
+            :func:`max`, :math:`\infty` for :func:`min`.
+
+        """
+        self.capacity = capacity
+        self.reducer = reducer
+        self.init_value = float(init_value)
+        self.depth = int(onp.ceil(onp.log2(capacity))) + 1  # the +1 is for the values themselves
+        self._arr = onp.full(shape=(2 ** self.depth - 1), fill_value=self.init_value)
+
+    @property
+    def root_value(self):
+        return self._arr[0]
+
+    @property
+    def array(self):
+        return self[...]
+
+    @array.setter
+    def array(self, new_values):
+        self[...] = new_values
+
+    def __array__(self):
+        return self.array
+
+    def __getitem__(self, idx):
+        idx = self._check_level_idx(self.depth - 1, idx, self.capacity)
+        return self._arr[idx]
+
+    def __setitem__(self, idx, values):
+        idx = self._check_level_idx(self.depth - 1, idx, self.capacity)
+        self._arr[idx] = values                                # update leaf-node values
+        level_offset = 2 ** (self.depth - 1) - 1               # number of nodes in higher levels
+        for level in range(self.depth - 2, -1, -1):
+            level_idx = onp.unique((idx - level_offset) // 2)  # level index (without offset)
+            left_child_idx = level_idx * 2 + level_offset      # indices for left children
+            right_child_idx = left_child_idx + 1               # indices for right children
+            level_offset = 2 ** level - 1
+            idx = level_idx + level_offset                     # parent indices
+            self._arr[idx] = self.reducer(self._arr[left_child_idx], self._arr[right_child_idx])
+
+    def partial_reduce(self, start=0, stop=None):
+        r"""
+
+        Reduce values over a partial range of indices.
+
+        Parameters
+        ----------
+        start : int or array of ints
+
+            The lower bound of the range (inclusive).
+
+        stop : int or array of ints, optional
+
+            The lower bound of the range (exclusive). If left unpsecified, this defaults to
+            :attr:`depth`.
+
+        Returns
+        -------
+        value : float
+
+            The result of the partial reduction.
+
+
+        """
+        # i and j are 1d arrays
+        i, j = self._check_start_stop_to_i_j(start, stop)
+
+        # trivial case
+        done = (i == j)
+        if done.all():
+            return self._arr[i]
+
+        # left/right accumulators (mask one of them to avoid over-counting if i == j)
+        a, b = self._arr[i], onp.where(done, self.init_value, self._arr[j])
+
+        # number of nodes in higher levels
+        level_offset = 2 ** (self.depth - 1) - 1
+
+        # we start from the leaves and work up towards the root
+        for level in range(self.depth - 2, -1, -1):
+
+            # get parent indices
+            level_offset_parent = 2 ** level - 1
+            i_parent = (i - level_offset) // 2 + level_offset_parent
+            j_parent = (j - level_offset) // 2 + level_offset_parent
+
+            # stop when we have a shared parent (possibly the root node, but not necessarily)
+            done |= (i_parent == j_parent)
+            if done.all():
+                return self.reducer(a, b)
+
+            # only accumulate right-child value if 'i' was a left child of 'i_parent'
+            a = onp.where((i % 2 == 1) & ~done, self.reducer(a, self._arr[i + 1]), a)
+
+            # only accumulate left-child value if 'j' was a right child of 'j_parent'
+            b = onp.where((j % 2 == 0) & ~done, self.reducer(b, self._arr[j - 1]), b)
+
+            # prepare for next loop
+            i, j, level_offset = i_parent, j_parent, level_offset_parent
+
+        assert False, 'this point should not be reached'
+
+    def __repr__(self):
+        s = ""
+        for level in range(self.depth):
+            idx = self._check_level_idx(level, ...)
+            s += f"\n  level={level} : {repr(self._arr[idx])}"
+        return f"{type(self).__name__}({s})"
+
+    def _check_level(self, level):
+        if level < -self.depth or level >= self.depth:
+            raise IndexError(f"tree level index {level} out of range; tree depth: {self.depth}")
+        return level % self.depth
+
+    def _check_level_idx(self, level, idx, level_size=None):
+        """ some boilerplate to transform an index relative to a tree level into a global index """
+        i = 2 ** level - 1                                    # level offset
+        n = 2 ** level if level_size is None else level_size  # level size
+
+        if idx is None or idx is Ellipsis:
+            return onp.arange(i, i + n)
+
+        if isinstance(idx, slice):
+            if not (idx.step is None or idx.step == 1):
+                raise NotImplementedError("reverse indexing not implemented; feature request?")
+            if not (idx.start is None or 0 <= idx.start < n):
+                raise IndexError(f"index slice.start out of range: {idx.start} not in [0, {n})")
+            if not (idx.stop is None or 0 < idx.stop <= n):
+                raise IndexError(f"index slice.stop out of range: {idx.stop} not in (0, {n}]")
+            start = i if idx.start is None else idx.start + i
+            stop = i + n if idx.stop is None else idx.stop + i
+            return onp.arange(start, stop)
+
+        idx = onp.asarray(idx, dtype='int32')
+        if onp.any(idx >= n) or onp.any(idx < -level_size):
+            raise IndexError("one of more entries in idx are out or range")
+        return idx % level_size + i  # offset
+
+    def _check_start_stop_to_i_j(self, start, stop):
+        """ some boiler plate to turn (start, stop) into left/right index arrays (i, j) """
+        start_orig, stop_orig = start, stop
+        if isinstance(start, int):
+            start = onp.array([start])
+        if not (isinstance(start, onp.ndarray)
+                and start.ndim == 1
+                and onp.issubdtype(start.dtype, onp.integer)):
+            raise TypeError("'start' must be an int or a 1d array of ints array")
+
+        if stop is None:
+            stop = onp.full_like(start, self.capacity)
+        if isinstance(stop, int):
+            stop = onp.full_like(start, stop)
+        if not (isinstance(stop, onp.ndarray)
+                and stop.ndim == 1
+                and onp.issubdtype(stop.dtype, onp.integer)):
+            raise TypeError("'stop' must be an int or a 1d array of ints array")
+
+        if start.size == 1 and stop.size > 1:
+            start = onp.full_like(stop, start[0])
+
+        if start.shape != stop.shape:
+            raise ValueError(
+                f"shapes must be equal, got: start.shape: {start.shape}, stop.shape: {stop.shape}")
+
+        i = self._check_level_idx(self.depth - 1, start, self.capacity)
+        j = self._check_level_idx(self.depth - 1, stop - 1, self.capacity)
+
+        if not onp.all(i <= j):
+            raise IndexError(
+                f"inconsistent ranges detected from (start, stop) = ({start_orig}, {stop_orig})")
+
+        return i, j
+
+
+class SumTree(SegmentTree):
+    def __init__(self, capacity):
+        super().__init__(capacity=capacity, reducer=onp.add, init_value=0)
+
+
+class MinTree(SegmentTree):
+    def __init__(self, capacity):
+        super().__init__(capacity=capacity, reducer=onp.minimum, init_value=float('inf'))
---
+++ coax/experience_replay/_segment_tree_test.py
@@ -0,0 +1,121 @@
+# ------------------------------------------------------------------------------------------------ #
+# MIT License                                                                                      #
+#                                                                                                  #
+# Copyright (c) 2020, Microsoft Corporation                                                        #
+#                                                                                                  #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software    #
+# and associated documentation files (the "Software"), to deal in the Software without             #
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,       #
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the    #
+# Software is furnished to do so, subject to the following conditions:                             #
+#                                                                                                  #
+# The above copyright notice and this permission notice shall be included in all copies or         #
+# substantial portions of the Software.                                                            #
+#                                                                                                  #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING    #
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND       #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,     #
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          #
+# ------------------------------------------------------------------------------------------------ #
+
+r"""
+
+This module was copied from the `OpenAI Baseline <https://github.com/openai/baselines>`_ project. It
+contains the data structures needed for efficient sampling and updating in `Prioritized Experience
+Replay <https://arxiv.org/abs/1511.05952>`_.
+
+"""
+
+import pytest
+import numpy as onp
+
+from ._segment_tree import SumTree, MinTree
+
+
+@pytest.fixture
+def sum_tree():
+    return SumTree(capacity=14)
+
+
+@pytest.fixture
+def min_tree():
+    tr = MinTree(capacity=8)
+    tr.array = onp.array([13, 7, 11, 17, 19, 5, 3, 23])
+    return tr
+
+
+def test_sum_tree_basic(sum_tree):
+    assert sum_tree.array.shape == (14,)
+    assert sum_tree.depth == 5
+    assert sum_tree.array.sum() == sum_tree.root_value == 0
+
+
+def test_min_tree_basic(min_tree):
+    assert min_tree.array.shape == (8,)
+    assert min_tree.depth == 4
+    assert min_tree.array.min() == min_tree.root_value == 3
+
+
+def test_set_values_emptyslice(sum_tree):
+    values = onp.arange(1, 15)
+    sum_tree[:] = values
+    assert sum_tree.array.sum() == sum_tree.root_value == values.sum() > 0
+
+
+def test_set_values_none(sum_tree):
+    values = onp.arange(1, 15)
+    sum_tree[None] = values
+    assert sum_tree.array.sum() == sum_tree.root_value == values.sum() > 0
+
+
+def test_set_values_ellipsis(sum_tree):
+    values = onp.arange(1, 15)
+    sum_tree[...] = values
+    assert sum_tree.array.sum() == sum_tree.root_value == values.sum() > 0
+
+
+def test_set_values_array(sum_tree):
+    values = onp.arange(1, 15)
+    sum_tree.array = values
+    assert sum_tree.array.sum() == sum_tree.root_value == values.sum() > 0
+
+
+def test_set_values_with_idx(sum_tree):
+    idx = onp.array([2, 6, 5, 12, 13])
+    values = onp.array([7., 13., 11., 17., 5.])
+    sum_tree[idx] = values
+    assert sum_tree.array.sum() == sum_tree.root_value == values.sum() > 0
+
+
+def test_partial_reduce_empty_range(sum_tree):
+    msg = r'inconsistent ranges detected from \(start, stop\) = \(1, 1\)'
+    with pytest.raises(IndexError, match=msg):
+        sum_tree.partial_reduce(1, 1)
+
+
+def test_partial_reduce_all(sum_tree):
+    values = onp.arange(1, 15)
+    sum_tree.array = values
+    assert sum_tree.partial_reduce() == sum_tree.array.sum() == sum_tree.root_value == values.sum()
+
+
+@pytest.mark.parametrize('i,j', [(1, 2), (13, 14), (3, 8), (0, None), (0, 3), (7, None)])
+def test_partial_reduce(sum_tree, i, j):
+    values = onp.arange(100, 114)
+    sum_tree.array = values
+    assert sum_tree.partial_reduce(i, j) == sum_tree.array[i:j].sum() == values[i:j].sum() > 0
+
+
+def test_partial_reduce_array_sum(sum_tree):
+    i, j = onp.array([0, 8, 3, 0, 0]), onp.array([1, 13, 14, 5, -1])
+    values = onp.arange(100, 114)
+    sum_tree.array = values
+    expected = onp.vectorize(lambda i, j: values[i:j].sum())
+    onp.testing.assert_allclose(sum_tree.partial_reduce(i, j), expected(i, j))
+
+
+def test_partial_reduce_array_min(min_tree):
+    i, j = onp.array([1, 6, 0]), onp.array([8, 7, 5])
+    expected = onp.vectorize(lambda i, j: min_tree.array[i:j].min())
+    onp.testing.assert_allclose(min_tree.partial_reduce(i, j), expected(i, j))
